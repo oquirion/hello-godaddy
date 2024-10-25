71aa04de6cd64d5099d6e43a0af78518702f9e5b
oquirion <olivierquirion@hotmail.com>
2024-10-24 21:45:43 -0400

    Added Deploy Step to workflow.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 // Define a route handler for the default home page
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, GoDaddy!');
 });
 
 // Start the server and listen on the specified port
