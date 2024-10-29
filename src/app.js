bdde904decfad3cd304197eb4daee96fb84d1801
oquirion <olivierquirion@hotmail.com>
2024-10-28 21:00:56 -0400

    New html page a little bit less ugly.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
 // Define a route handler for the default home page
 app.get('/', (req, res) => {
-  res.send("Hello, GoDaddy!<br>Looking forward to working with you!");
+  const filePath = path.join(__dirname, 'public', 'index.html');
+  res.send(filePath);
 });
 
 // Start the server and listen on the specified port
